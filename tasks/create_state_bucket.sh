28fd94ad5065aa78d2c4e3d86f7cae65ddafc201
Jessica Stenning <jessica.stenning@engineerbetter.com>
2019-09-22 17:21:47 +0100

    adds create_state_bucket task

---
+++ tasks/create_state_bucket.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+buckets="$(aws s3 ls)"
+
+
+if [[ $buckets == *"${PROJECT_ID}"* ]]; then
+  echo "Existing bucket found for project ${PROJECT_ID}"
+
+  if aws s3 ls "s3://${PROJECT_ID}/ci/terraform.tfstate"; then
+    aws s3 cp "s3://${PROJECT_ID}/ci/terraform.tfstate" terraform.tfstate
+  else
+    echo "Error - project bucket exists, but ci/terraform.tfstate does not"
+    exit 1
+  fi
+else
+  echo "No existing bucket found for project ${PROJECT_ID}, assuming first run"
+fi
+
+pushd offline_cs/eb_infra/tfstate_bucket/
+  terraform init
+
+  terraform apply \
+  -auto-approve \
+  -input=false \
+  -state=../../../terraform.tfstate \
+  -state-out=../../../tfstate-out/terraform.tfstate
+popd
