8b2ee3bd35280d965de0610437216f013768062e
Jessica Stenning <jessica.stenning@engineerbetter.com>
2019-09-22 20:12:31 +0100

    corrects variable name in create_state_bucket script

--- tasks/create_state_bucket.sh
+++ tasks/create_state_bucket.sh
@@ -5,17 +5,17 @@ set -euo pipefail
 buckets="$(aws s3 ls)"
 
 
-if [[ $buckets == *"${PROJECT_ID}"* ]]; then
-  echo "Existing bucket found for project ${PROJECT_ID}"
+if [[ $buckets == *"${BUCKET_NAME}"* ]]; then
+  echo "Existing bucket found for project ${BUCKET_NAME}"
 
-  if aws s3 ls "s3://${PROJECT_ID}/ci/terraform.tfstate"; then
-    aws s3 cp "s3://${PROJECT_ID}/ci/terraform.tfstate" terraform.tfstate
+  if aws s3 ls "s3://${BUCKET_NAME}/ci/terraform.tfstate"; then
+    aws s3 cp "s3://${BUCKET_NAME}/ci/terraform.tfstate" terraform.tfstate
   else
     echo "Error - project bucket exists, but ci/terraform.tfstate does not"
     exit 1
   fi
 else
-  echo "No existing bucket found for project ${PROJECT_ID}, assuming first run"
+  echo "No existing bucket found for project ${BUCKET_NAME}, assuming first run"
 fi
 
 pushd git-repo/tfstate_bucket/
